780b123b27dbe855583858319a2ab81b4a5ab999
GabriBarto <gabri4216@gmail.com>
2026-07-24 12:02:47 +0200

    setup librerie

---
+++ bot.py
@@ -0,0 +1,6 @@
+import logging
+from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
+from telegram.ext import Application, CommandHandler, MessageHandler, CallbackQueryHandler, filters, ContextTypes
+
+TOKEN = "8836413586:AAFnyACjcLPCqva28o8LhLVN39mNTi4lzdc"
+
