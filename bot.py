57598f1eb56ca93920a3da3ceb20c902b5eb3874
Gabriele Bartolini <121085749+GabriBarto@users.noreply.github.com>
2026-07-24 12:34:59 +0200

    test
    test

--- bot.py
+++ bot.py
@@ -1,6 +1,2 @@
-import logging
-from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
-from telegram.ext import Application, CommandHandler, MessageHandler, CallbackQueryHandler, filters, ContextTypes
 
-TOKEN = "8836413586:AAFnyACjcLPCqva28o8LhLVN39mNTi4lzdc"
 
